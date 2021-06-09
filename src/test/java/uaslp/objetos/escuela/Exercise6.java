d7e909f6ec59fbec4cb57a73c9a9ecd6f37f23c8
Abigail0015 <A298360@alumnos.uaslp.mx>
2021-06-09 09:58:39 -0500

    EJERCICIO 6 TAREA FINAL

--- src/test/java/uaslp/objetos/escuela/Exercise6.java
+++ src/test/java/uaslp/objetos/escuela/Exercise6.java
@@ -1,6 +1,7 @@
 package uaslp.objetos.escuela;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -25,4 +26,61 @@ public class Exercise6 {
         assertThat(Dependencia2.class).isInterface();
         assertThat(Dependencia3.class).isInterface();
     }
+
+    @Test
+    public void validarAIgualB(){
+        int a = 1;
+        int b = 1;
+        String c = "";
+
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+        algoritmoX.algoritmoACubrir(a,b,c);
+
+        Mockito.verifyNoMoreInteractions(dependencia1,dependencia2,dependencia3);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verify(dependencia1).save(c);
+
+    }
+
+    @Test
+    public void validarAMayorQueB(){
+        //Given
+        int a = 2;
+        int b = 1;
+        String c = "";
+
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+        algoritmoX.algoritmoACubrir(a,b,c);
+
+        Mockito.verifyNoMoreInteractions(dependencia1,dependencia2,dependencia3);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verify(dependencia3).send(a,b);
+
+    }
+
+    @Test
+    public void ValidarBMayorQueA(){
+
+        int a = 1;
+        int b = 2;
+        String c = "";
+
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+        algoritmoX.algoritmoACubrir(a,b,c);
+
+        Mockito.verifyNoMoreInteractions(dependencia1,dependencia2,dependencia3);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verify(dependencia2).print(a, c);
+    }
+
+
 }
